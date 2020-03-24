c684f0d8a142b99e4a959f8acd1cc4af0805dbf8
Anil Yildirim <anily@umich.edu>
2020-03-24 11:58:29 -0400

    removed some unused code

--- omfsi/as_scenario.py
+++ omfsi/as_scenario.py
@@ -21,8 +21,6 @@ class AS_Scenario(om.Group):
         aero = self.aero_builder.get_element()
         struct = self.struct_builder.get_element()
         disp_xfer, load_xfer = self.xfer_builder.get_element()
-        # component that only adds a geoemtric displacement to the aero surface. might remove
-        geo_disp = Geo_Disp()
 
         # add the subgroups
         self.add_subsystem('disp_xfer', disp_xfer)
@@ -42,28 +40,3 @@ class AS_Scenario(om.Group):
         self.connect('aero.f_a', 'load_xfer.f_a')
         self.connect('load_xfer.f_s', 'struct.f_s')
         self.connect('struct.u_s', ['disp_xfer.u_s', 'load_xfer.u_s'])
-
-    def set_ap(self, ap):
-        # this function sets the aero problem in all relevant spots
-        # and adds the DVs of the aero problem
-
-        # call the set_ap function on every component that uses the ap
-        self.fsi_group.aero.solver.set_ap(ap)
-        self.fsi_group.aero.force.set_ap(ap)
-        self.aero_funcs.set_ap(ap)
-
-        # connect the AP DVs to all components that use them
-        self.ap_vars,_ = get_dvs_and_cons(ap=ap)
-        for (args, kwargs) in self.ap_vars:
-            name = args[0]
-            size = args[1]
-
-            self.dv.add_output(name, val=kwargs['value'])
-
-            ap_vars_target = [
-                'fsi_group.aero.solver.%s'%name,
-                'fsi_group.aero.force.%s'%name,
-                'aero_funcs.%s'%name
-            ]
-
-            self.connect('dv.%s'%name, ap_vars_target)
