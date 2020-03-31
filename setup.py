e7ff70708ac50023719c763f191b64ec6e7595ec
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2020-03-31 13:36:47 -0400

    Update root directory for name change

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-__package_name__ = "omfsi"
+__package_name__ = "mphys"
 __package_version__ = "0.1.0"
 
 
 setup(
     name=__package_name__,
     version=__package_version__,
-    description=("Components and related code for fluid-structure interaction"
+    description=("Components and related code for multiphysics"
         " problems in OpenMDAO"),
     author="",
     author_email="",
