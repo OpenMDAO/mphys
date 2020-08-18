851fb38a455c0054ed35caf702724ae0ca40dea2
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2020-08-17 21:31:46 -0400

    Use mphys' geodisp instead of recoding

--- mphys/mphys_fun3d.py
+++ mphys/mphys_fun3d.py
@@ -9,6 +9,7 @@ from libmeshdef.meshdef_parfait import MeshDeformation
 from sfe.sfe_parfait import SFE
 from sfe.sfe_openmdao import SfeSolverOpenMdao, SfeForcesOpenMdao
 from mphys.integrated_forces import IntegratedSurfaceForces
+from mphys.geo_disp import GeoDisp
 
 class Fun3dMesh(om.ExplicitComponent):
     def initialize(self):
@@ -48,52 +49,6 @@ class Fun3dMesh(om.ExplicitComponent):
             if 'x_a0_points' in d_inputs:
                 d_inputs['x_a0_points'] += d_outputs['x_a0']
 
-class GeoDisp(ExplicitComponent):
-    """
-    This component adds the aerodynamic
-    displacements to the geometry-changed aerodynamic surface
-    """
-    def initialize(self):
-        self.options['distributed'] = True
-        self.options.declare('nnodes')
-
-    def setup(self):
-        aero_nnodes = self.options['nnodes']
-        local_size = aero_nnodes * 3
-        n_list = self.comm.allgather(local_size)
-        irank  = self.comm.rank
-
-        n1 = np.sum(n_list[:irank])
-        n2 = np.sum(n_list[:irank+1])
-
-        self.add_input('x_a0', shape=local_size,
-                               src_indices=np.arange(n1,n2,dtype=int),
-                               desc='aerodynamic surface with geom changes')
-        self.add_input('u_a', shape=local_size,
-                              val=np.zeros(local_size),
-                              src_indices=np.arange(n1,n2,dtype=int),
-                              desc='aerodynamic surface displacements')
-
-        self.add_output('x_a', shape=local_size,
-                               desc='deformed aerodynamic surface')
-
-    def compute(self,inputs,outputs):
-        outputs['x_a'] = inputs['x_a0'] + inputs['u_a']
-
-    def compute_jacvec_product(self,inputs,d_inputs,d_outputs,mode):
-        if mode == 'fwd':
-            if 'x_a' in d_outputs:
-                if 'x_a0' in d_inputs:
-                    d_outputs['x_a'] += d_inputs['x_a0']
-                if 'u_a' in d_inputs:
-                    d_outputs['x_a'] += d_inputs['u_a']
-        if mode == 'rev':
-            if 'x_a' in d_outputs:
-                if 'x_a0' in d_inputs:
-                    d_inputs['x_a0'] += d_outputs['x_a']
-                if 'u_a' in d_inputs:
-                    d_inputs['u_a']  += d_outputs['x_a']
-
 class Fun3dFsiSolverGroup(om.Group):
     def initialize(self):
         self.options.declare('meshdef_comp')
@@ -102,7 +57,7 @@ class Fun3dFsiSolverGroup(om.Group):
         self.options.declare('number_of_surface_nodes')
 
     def setup(self):
-        geodisp_comp = GeoDisp(self.options['number_of_surface_nodes'])
+        geodisp_comp = GeoDisp(number_of_surface_nodes=self.options['number_of_surface_nodes'])
         meshdef_comp = self.options['meshdef_comp']
         flow_comp = self.options['flow_comp']
         forces_comp = self.options['forces_comp']
