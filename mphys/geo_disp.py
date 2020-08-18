2074e62f58eccef8c6f2de231157824e233c6936
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2020-08-17 21:13:16 -0400

    Pep 8 GeoDisp class name and break long lines

--- mphys/geo_disp.py
+++ mphys/geo_disp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import openmdao.api as om
 
-class Geo_Disp(om.ExplicitComponent):
+class GeoDisp(om.ExplicitComponent):
     """
     This component adds the aerodynamic
     displacements to the geometry-changed aerodynamic surface
@@ -19,10 +19,15 @@ class Geo_Disp(om.ExplicitComponent):
         n1 = np.sum(n_list[:irank])
         n2 = np.sum(n_list[:irank+1])
 
-        self.add_input('x_a0',shape=local_size,src_indices=np.arange(n1,n2,dtype=int),desc='aerodynamic surface with geom changes')
-        self.add_input('u_a', shape=local_size,val=np.zeros(local_size),src_indices=np.arange(n1,n2,dtype=int),desc='aerodynamic surface displacements')
+        self.add_input('x_a0', shape=local_size,
+                               src_indices=np.arange(n1,n2,dtype=int),
+                               desc='aerodynamic surface with geom changes')
+        self.add_input('u_a',  shape=local_size,
+                               val=np.zeros(local_size),
+                               src_indices=np.arange(n1,n2,dtype=int),
+                               desc='aerodynamic surface displacements')
 
-        self.add_output('x_a',shape=local_size,desc='deformed aerodynamic surface')
+        self.add_output('x_a', shape=local_size, desc='deformed aerodynamic surface')
 
     def compute(self,inputs,outputs):
         outputs['x_a'] = inputs['x_a0'] + inputs['u_a']
