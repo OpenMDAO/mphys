ad41506678cf241ab43bc5193caadda7f965b23d
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2021-03-05 21:47:45 -0500

    Add parallel option to struct only scenarion and an untested geometry group

--- mphys/scenario_structural.py
+++ mphys/scenario_structural.py
@@ -4,9 +4,39 @@ class ScenarioStructural(Scenario):
 
     def initialize(self):
         self.options.declare('struct_builder', recordable=False)
+        self.options.declare('in_MultipointParallelGroup', default=False)
 
     def setup(self):
         struct_builder = self.options['struct_builder']
+
+        if self.options['in_MultipointParallelGroup']:
+            struct_builder.initialize(self.comm)
+            self.mphys_add_subsystem('mesh',struct_builder.get_mesh_coordinate_subsystem())
+
+        self.mphys_add_pre_coupling_subsystem('struct', struct_builder)
+        self.mphys_add_subsystem('coupling',struct_builder.get_coupling_group_subsystem())
+        self.mphys_add_post_coupling_subsystem('struct', struct_builder)
+
+
+# UNTESTED: to show in_MultipointParallelGroup option isn't necessary and add geometry
+class ScenarioStructuralParallelGeometry(Scenario):
+
+    def initialize(self):
+        self.options.declare('struct_builder', recordable=False)
+        self.options.declare('geometry_builder',recordable=False)
+
+    def setup(self):
+        struct_builder = self.options['struct_builder']
+        geometry_builder = self.options['geometry_builder']
+
+        struct_builder.initialize(self.comm)
+        geometry_builder.initialize(self.comm)
+
+        # don't use mphys_add_subsystem for mesh so that the geometry's coordinate output
+        # are promoted, not the mesh
+        self.add_subsystem('mesh',struct_builder.get_mesh_coordinate_subsystem())
+        self.mphys_add_subsystem('geometry',geometry_builder.get_mesh_coordinate_subsystem())
+        self.connect('mesh.x_struct0','geometry.x_struct_in')
 
         self.mphys_add_pre_coupling_subsystem('struct', struct_builder)
         self.mphys_add_subsystem('coupling',struct_builder.get_coupling_group_subsystem())
