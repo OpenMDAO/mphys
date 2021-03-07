439cfba4e6b2f0c68e5c9af567fcd7804beb1b95
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2021-03-07 13:47:24 -0500

    Add fun3d version of aero_only

---
+++ examples/aero_only/run_fun3d.py
@@ -0,0 +1,47 @@
+
+from mpi4py import MPI
+import openmdao.api as om
+
+from mphys import Multipoint
+from mphys.mphys_fun3d import Fun3dSfeBuilder
+from mphys.scenario_aero import ScenarioAero
+
+
+class Top(Multipoint):
+    def setup(self):
+        # FUN3D options
+        project_rootname = 'agard_debug'
+        boundary_tags = [3]
+
+        mach = 0.5,
+        aoa = 0.0
+        yaw = 0.0
+        q_inf = 3000.
+        reynolds = 1.0
+
+        dvs = self.add_subsystem('dvs', om.IndepVarComp(), promotes=['*'])
+        dvs.add_output('aoa', val=aoa, units='deg')
+        dvs.add_output('yaw', val=yaw, units='deg')
+        dvs.add_output('mach', mach)
+        dvs.add_output('q_inf', q_inf)
+        dvs.add_output('reynolds', reynolds)
+
+        aero_builder = Fun3dSfeBuilder(project_rootname, boundary_tags)
+        aero_builder.initialize(self.comm)
+
+        self.add_subsystem('mesh',aero_builder.get_mesh_coordinate_subsystem())
+        self.mphys_add_scenario('cruise',ScenarioAero(aero_builder=aero_builder))
+        self.connect('mesh.x_aero0','cruise.x_aero')
+
+        for dv in ['aoa', 'yaw', 'mach', 'q_inf','reynolds']:
+            self.connect(dv, f'cruise.{dv}')
+
+prob = om.Problem()
+prob.model = Top()
+prob.setup()
+
+om.n2(prob, show_browser=False, outfile='mphys_fun3d.html')
+
+prob.run_model()
+if MPI.COMM_WORLD.rank == 0:
+    print('C_L, C_D =',prob['cruise.C_L'], prob['cruise.C_D'])
