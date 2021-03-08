b11eb2fd8fbc3d98bbd263467022bcb5890c8663
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2021-03-08 09:23:24 -0500

    Add parallel FSI example script

--- mphys/scenario_aerostructural.py
+++ mphys/scenario_aerostructural.py
@@ -2,17 +2,27 @@ import openmdao.api as om
 from .scenario import Scenario
 from .coupling_aerostructural import CouplingAeroStructural
 
+
 class ScenarioAeroStructural(Scenario):
 
     def initialize(self):
         self.options.declare('aero_builder', recordable=False)
         self.options.declare('struct_builder', recordable=False)
         self.options.declare('ldxfer_builder', recordable=False)
+        self.options.declare('geometry_builder', default=None, recordable=False)
+        self.options.declare('in_MultipointParallel', default=False)
 
     def setup(self):
         aero_builder = self.options['aero_builder']
         struct_builder = self.options['struct_builder']
         ldxfer_builder = self.options['ldxfer_builder']
+        geometry_builder = self.options['geometry_builder']
+
+        if self.options['in_MultipointParallel']:
+            self._mphys_initialize_builders(aero_builder, struct_builder,
+                                            ldxfer_builder, geometry_builder)
+            self._mphys_add_mesh_and_geometry_subsystems(aero_builder, struct_builder,
+                                                         geometry_builder)
 
         self.mphys_add_pre_coupling_subsystem('aero', aero_builder)
         self.mphys_add_pre_coupling_subsystem('struct', struct_builder)
@@ -21,11 +31,37 @@ class ScenarioAeroStructural(Scenario):
         coupling_group = CouplingAeroStructural(aero_builder=aero_builder,
                                                 struct_builder=struct_builder,
                                                 ldxfer_builder=ldxfer_builder)
-        self.mphys_add_subsystem('coupling',coupling_group)
+        self.mphys_add_subsystem('coupling', coupling_group)
 
         self.mphys_add_post_coupling_subsystem('aero', aero_builder)
         self.mphys_add_post_coupling_subsystem('struct', struct_builder)
         self.mphys_add_post_coupling_subsystem('ldxfer', ldxfer_builder)
 
-        self.nonlinear_solver = om.NonlinearBlockGS(maxiter=25, iprint=2, atol=1e-8, rtol=1e-8, use_aitken=True)
-        self.linear_solver = om.LinearBlockGS(maxiter=25, iprint=2, atol=1e-8, rtol=1e-8, use_aitken=True)
+        self.nonlinear_solver = om.NonlinearBlockGS(maxiter=25, iprint=2,
+                                                    atol=1e-8, rtol=1e-8,
+                                                    use_aitken=True)
+        self.linear_solver = om.LinearBlockGS(maxiter=25, iprint=2,
+                                              atol=1e-8, rtol=1e-8,
+                                              use_aitken=True)
+
+    def _mphys_initialize_builders(self, aero_builder, struct_builder,
+                                   ldxfer_builder, geometry_builder):
+        aero_builder.initialize(self.comm)
+        struct_builder.initialize(self.comm)
+        ldxfer_builder.initialize(self.comm)
+        if geometry_builder is not None:
+            geometry_builder.initialize(self.comm)
+
+    def _mphys_add_mesh_and_geometry_subsystems(self, aero_builder, struct_builder,
+                                                geometry_builder):
+        if geometry_builder is None:
+            self.mphys_add_subsystem('mesh_aero', aero_builder.get_mesh_coordinate_subsystem())
+            self.mphys_add_subsystem('mesh_struct', struct_builder.get_mesh_coordinate_subsystem())
+
+        else:
+            self.add_subsystem('mesh_aero', aero_builder.get_mesh_coordinate_subsystem())
+            self.add_subsystem('mesh_struct', struct_builder.get_mesh_coordinate_subsystem())
+            self.mphys_add_subsystem('geometry', geometry_builder.get_mesh_coordinate_subsystem())
+
+            self.connect('mesh_aero.x_aero0', 'geometry.x_aero_in')
+            self.connect('mesh_struct.x_struct0', 'geometry.x_struct_in')
