277ac6b370f82a689db4d589f76aeca1a45ac81a
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2021-03-19 09:07:23 -0400

    Put common methods into a class

--- tests/unit_tests/common_methods.py
+++ tests/unit_tests/common_methods.py
@@ -2,11 +2,11 @@
 Have a single implementation of tests that many unit tests will run
 """
 
+class CommonMethods:
 
-def test_run_model(self):
-    self.prob.run_model()
+    def test_run_model(self, obj):
+        obj.prob.run_model()
 
-
-def test_no_autoivcs(self):
-    for output in self.prob.model._conn_global_abs_in2out.values():
-        self.assertFalse('auto_ivc' in output)
+    def test_no_autoivcs(self, obj):
+        for output in obj.prob.model._conn_global_abs_in2out.values():
+            obj.assertFalse('auto_ivc' in output)
