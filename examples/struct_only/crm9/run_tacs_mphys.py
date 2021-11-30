2692e3189cd5eb8bb11b07df875fa6d41803252c
trbrooks <tim.r.brooks@nasa.gov>
2021-11-30 13:02:53 -0500

    Updating tacs struct_only example

--- examples/struct_only/crm9/run_tacs_mphys.py
+++ examples/struct_only/crm9/run_tacs_mphys.py
@@ -1,6 +1,5 @@
 """
 Mass minimization of uCRM wingbox subject to a constant vertical force
-
 """
 from __future__ import division, print_function
 
@@ -32,48 +31,31 @@ class Top(Multipoint):
         self.connect('dv_struct', 'analysis.dv_struct')
 
     def configure(self):
-        # create the aero problems for both analysis point.
-        # this is custom to the ADflow based approach we chose here.
+        # create the tacs problems for analysis point.
+        # this is custom to the tacs based approach we chose here.
         # any solver can have their own custom approach here, and we don't
         # need to use a common API. AND, if we wanted to define a common API,
         # it can easily be defined on the mp group, or the aero group.
         fea_solver = self.analysis.coupling.fea_solver
 
         # ==============================================================================
-        # Setup structural problem
+        # Setup static problem
         # ==============================================================================
-        # Structural problem
-        sp = fea_solver.createStaticProblem(name='cruise', options={'printTiming':True})
+        # Static problem
+        sp = fea_solver.createStaticProblem(name='analysis', options={'printTiming':True})
         # Add TACS Functions
         sp.addFunction('mass', functions.StructuralMass)
         sp.addFunction('ks_vmfailure', functions.KSFailure, safetyFactor=1.0,
                        ksWeight=100.0)
-        '''
-        # Various methods for adding loads to structural problem:
-        # Let's model an engine load of 75kN weight, and 64kN thrust attached to spar
-        compIDs = fea_solver.selectCompIDs(["WING_SPARS/LE_SPAR/SEG.16", "WING_SPARS/LE_SPAR/SEG.17"])
-        We = 75.0e3  # N
-        Te = 64.0e3  # N
-        sp.addLoadToComponents(compIDs, [-Te, 0.0, -We, 0.0, 0.0, 0.0], averageLoad=True)
-        # Next we'll approximate aerodynamic loads on upper/lower skin with a uniform traction
-        L = 3e3  # N/m^2
-        D = 150  # N/m^2
-        tracVec = np.array([D, 0.0, L])
-        compIDs = fea_solver.selectCompIDs(include='SKIN')
-        sp.addTractionToComponents(compIDs, tracVec)
-        # Finally, we can approximate fuel load by adding a pressure load to the lower skin
-        P = 2e3  # N/m^2
-        compIDs = fea_solver.selectCompIDs(include='L_SKIN')
-        sp.addPressureToComponents(compIDs, P)
-        '''
-        F = sp.F.getArray()
+
+        # Add forces to static problem
+        F = fea_solver.createVec()
         ndof = fea_solver.getVarsPerNode()
         F[2::ndof] = 100.0
+        sp.addLoadToRHS(F)
 
-        # here we set the aero problems for every cruise case we have.
-        # this can also be called set_flow_conditions, we don't need to create and pass an AP,
-        # just flow conditions is probably a better general API
-        # this call automatically adds the DVs for the respective scenario
+        # here we set the tacs problems for the analysis case we have.
+        # this call automatically adds the functions and loads for the respective scenario
         self.analysis.coupling.mphys_set_sp(sp)
         self.analysis.struct_post.mphys_set_sp(sp)
 
@@ -90,17 +72,13 @@ model.add_design_var('dv_struct', lower=0.002, upper=0.2, scaler=1000.0)
 model.add_objective('analysis.struct_post.mass', index=0, scaler=1.0 / 1000.0)
 model.add_constraint('analysis.struct_post.ks_vmfailure', lower=0.0, upper=1.0, scaler=1.0)
 
-#prob.driver = om.ScipyOptimizeDriver(debug_print=['objs', 'nl_cons'], maxiter=100)
-#prob.driver.options['optimizer'] = 'SLSQP'
-
-prob.driver = om.pyOptSparseDriver()
-prob.driver.options['optimizer'] = "SNOPT"
-prob.driver.opt_settings['Major iterations limit'] = 200
+prob.driver = om.ScipyOptimizeDriver(debug_print=['objs', 'nl_cons'], maxiter=1)
+prob.driver.options['optimizer'] = 'SLSQP'
 
 prob.setup()
 om.n2(prob, show_browser=False, outfile='tacs_struct.html')
 prob.run_model()
 
 prob.run_driver()
-for i in range(240):
+for i in range(242):
     print('final dvs', i, prob['dv_struct'][i])
