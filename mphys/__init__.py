7369f42107e385483f247f0a4d939b22417e687e
trbrooks <tim.r.brooks@nasa.gov>
2022-01-26 16:46:15 -0500

    adding masked_converter to unit tests

--- mphys/__init__.py
+++ mphys/__init__.py
@@ -2,3 +2,4 @@
 from .builder import Builder
 from .multipoint import Multipoint, MultipointParallel
 from .distributed_converter import DistributedConverter, DistributedVariableDescription
+from .mask_converter import MaskedConverter, UnmaskedConverter, MaskedVariableDescription
