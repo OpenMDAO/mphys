cdb3f27f8e7152bdabcbee3ede125f5d22f686d2
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2024-04-02 14:44:32 -0400

    Missed a file from previous commit

--- mphys/core/__init__.py
+++ mphys/core/__init__.py
@@ -5,6 +5,6 @@ from .coupling_group import CouplingGroup
 from .distributed_converter import DistributedConverter, DistributedVariableDescription
 from .mask_converter import MaskedConverter, UnmaskedConverter, MaskedVariableDescription
 from .multipoint import Multipoint, MultipointParallel
-from .mphys_group import MphysGroup
+from .mphys_group import MPhysGroup
 from .scenario import Scenario
 from .variable_convention import MPhysVariables
