a282a04740577bdb7afcae265144b2acfeb9e404
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2025-03-10 12:26:57 -0400

    Use abs error in unit tests

--- tests/unit_tests/test_distributed_converter.py
+++ tests/unit_tests/test_distributed_converter.py
@@ -70,21 +70,13 @@ class TestDistributedConverter(unittest.TestCase):
         tol = 1e-9
         for in_var in ["in1", "in2"]:
             err = partials["converter"][(f"{in_var}_serial", in_var)]
-            # If fd check magnitude is exactly zero, use abs tol
-            if err["magnitude"].fd == 0.0:
-                check_error = err["abs error"]
-            else:
-                check_error = err["rel error"]
+            check_error = err["abs error"]
             assert_near_equal(check_error.reverse, 0.0, tolerance=tol)
             assert_near_equal(check_error.forward, 0.0, tolerance=tol)
             assert_near_equal(check_error.fwd_rev, 0.0, tolerance=tol)
         for out_var in ["out1", "out2"]:
             err = partials["converter"][(out_var, f"{out_var}_serial")]
-            # If fd check magnitude is exactly zero, use abs tol
-            if err["magnitude"].fd == 0.0:
-                check_error = err["abs error"]
-            else:
-                check_error = err["rel error"]
+            check_error = err["abs error"]
             assert_near_equal(check_error.reverse, 0.0, tolerance=tol)
             assert_near_equal(check_error.forward, 0.0, tolerance=tol)
             assert_near_equal(check_error.fwd_rev, 0.0, tolerance=tol)
